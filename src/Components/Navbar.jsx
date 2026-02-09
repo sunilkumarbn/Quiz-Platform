1c9ff9af2901b52c2b3d5c19ec39df84494d73cf
ADARSH B C <adarshadarshreddy123@gmail.com>
2026-02-09 20:42:45 +0530

    added css for navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import './Navbar.css'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-    </div>
-  )
+  <nav className="navbar-container">
+    <Link to="/" className="nav-link">Home</Link>
+    <Link to="/about" className="nav-link">About</Link>
+    <Link to="/contact" className="nav-link">Contact</Link>
+  </nav>
+)
+  
 }
 
 export default Navbar
