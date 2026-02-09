7c240c410939f730595ea5d881274c008467c0c2
sunilkumarbn <sunilkumarbn800@gmail.com>
2026-02-09 18:17:45 +0530

    Fixed Layout import Error

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Components/Home'
 import About from './Components/About'
 import Contact from './Components/Contact'  
+import Layout from './Components/Layout'
 import './App.css'
 
 const App = () => {
