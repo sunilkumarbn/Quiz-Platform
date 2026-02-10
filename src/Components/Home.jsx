2529ee414d47ac78d80f5d9fc87f25b61daf6190
sunilkumarbn <sunilkumarbn800@gmail.com>
2026-02-10 13:46:57 +0530

    Optimize Css for Card Component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Card from './Card'
 const Home = () => {
   return (
-    <div>
-        <h1>Welcome to the Quiz Platform</h1>
+    <>
+    <h1>Welcome to the Quiz Platform</h1>
+    <div className='card-wrapper'>
         <Card title="Create Your Own Quiz" description="Easily create quizzes on any topic and share them with friends!" />
         <Card title="Take Quizzes" description="Test your knowledge by taking quizzes created by others!" />
         <Card title="Track Your Progress" description="Keep track of your quiz scores and see how you improve over time!" />
     </div>
+    </>
   )
 }
 
