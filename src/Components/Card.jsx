442c8f6be5f74efae3891f5f170aa052fe9e3f32
ADARSH B C <adarshadarshreddy123@gmail.com>
2026-02-10 10:55:57 +0530

    upadated Card css

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./Card.css";
 const Card = ({ title, description }) => {
   return (
     <div className="card">
-      <h3>{title}</h3>
+      <h3>{title }</h3>
       <p>{description}</p>
       <button>Get Started</button>
     </div>
